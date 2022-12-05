433955b6eed379eeb5c01bcf22493e2cac7cbbc1
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2022-12-05 10:49:37 +0100

    Day 5

---
+++ 05/5a.py
@@ -0,0 +1,45 @@
+with open("input.txt", "r") as f:
+    FILE = f.read()
+    STACK_STR, INSTRUCTIONS_STR = FILE.split("\n\n", 1)
+    STACK, INSTRUCTIONS = STACK_STR.splitlines(), INSTRUCTIONS_STR.splitlines()
+
+
+def get_stacks():
+    stacks = []
+
+    STACK.pop()  # Remove the stack numbers
+    while STACK:
+        line = STACK.pop(0)
+        line_elements = []
+
+        while line:
+            value = line[:3].strip(" []")
+            line_elements.append(value)
+            line = line[4:]
+
+        if not stacks:
+            stacks = [[] for _ in range(len(line_elements))]
+
+        for i, value in enumerate(line_elements):
+            if value:
+                stacks[i].append(value)
+
+    for stack in stacks:
+        stack.reverse()
+
+    return stacks
+
+
+def do_move(stacks, from_, to, qty):
+    for _ in range(qty):
+        stacks[to - 1].append(stacks[from_ - 1].pop())
+
+
+stacks = get_stacks()
+for instruction in INSTRUCTIONS:
+    _, qty, _, from_, _, to = instruction.split()
+    do_move(stacks, int(from_), int(to), int(qty))
+
+
+for stack in stacks:
+    print(stack[-1], end='')
